1e76e4fd6efd733c69400e9f6031540d7808966b
Mohsinalisyed <113241363+Mohsinalisyed@users.noreply.github.com>
2024-02-21 01:41:13 +0500

    sidbar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,6 +4,8 @@ import { Sidebar, MenuItem } from 'react-pro-sidebar';
 import Toggle from '../../theme/Toggoler';
 import { StyledMenu } from './style';
 import { HeadingS } from '../../style';
+import LogoDark from '../../lib/Svg/LogoDark';
+import LogoLight from '../../lib/Svg/LogoLight';
 interface ISidebar {
     theme: string
     toggleTheme: () => void
@@ -11,20 +13,22 @@ interface ISidebar {
 const MainSidebar: React.FC<ISidebar> = ({ theme, toggleTheme }) => {
     return (
         <Sidebar style={{
-            border: "none", display: "flex",
+            border: "none",
+            display: "flex",
             alignItems: "space-between",
             flexDirection: "column",
-            background: theme === 'light' ? "#fff" : "white",
-            paddingBottom: "40px"
         }}>
-            <StyledMenu >
-                <HeadingS style={{ marginTop: "0" }}>All Boards (3)</HeadingS>
-                <MenuItem>Documentation</MenuItem>
-                <MenuItem>Calendar</MenuItem>
-                <MenuItem>Documentation</MenuItem>
-                <MenuItem>Calendar</MenuItem>
-            </StyledMenu>
-            <Toggle theme={theme} toggleTheme={toggleTheme} />
+            <div style={{ background: theme === 'light' ? "#fff" : "#363537", }} >
+                <div>{theme === 'light' ? <LogoDark /> : <LogoLight />}</div>
+                <StyledMenu >
+                    <HeadingS style={{ margin: "0" }}>All Boards (3)</HeadingS>
+                    <MenuItem>Documentation</MenuItem>
+                    <MenuItem>Calendar</MenuItem>
+                    <MenuItem>Documentation</MenuItem>
+                    <MenuItem>Calendar</MenuItem>
+                </StyledMenu>
+                <Toggle theme={theme} toggleTheme={toggleTheme} />
+            </div>
         </Sidebar >
     );
 };
--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -3,4 +3,10 @@ import styled from "styled-components";
 
 export const StyledMenu = styled(Menu)`
   height: 90vh;
+  li {
+    padding: 0px;
+  }
+  .ps-menu-button {
+    padding: 0 !important;
+  }
 `;
