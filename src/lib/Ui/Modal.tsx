0846354206ef2e00615d22e1d77ab1cb764b66e1
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-02-21 06:12:45 -0700

    modal

--- src/lib/Ui/Modal.tsx
+++ src/lib/Ui/Modal.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import styled from "styled-components";
 import { darkTheme, lightTheme } from "../../theme/Theme";
-import { HeadingS, HeadingXL } from "../../style";
+import { HeadingM, HeadingS, HeadingXL } from "../../style";
 
 const Background = styled.div`
   width: 100%;
@@ -22,12 +22,12 @@ const ModalWrapper = styled.div`
   position: relative;
   z-index: 10000;
   border-radius: 10px;
-  padding:24px;
+  padding:32px;
 `;
 
 
 const ModalContent = styled.div`
-  display: flex;
+  display: block;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -68,16 +68,19 @@ export const Modal: React.FC<ModalProps> = ({ showModal, setShowModal, theme }) 
             {showModal ? (
           <Background ref={modalRef} onClick={closeModal}>
             <ModalWrapper style={{ background: themeMode.modalbg }}>
-                            <ModalContent>
+                <ModalContent>
                 <HeadingXL style={{ color: themeMode.text }}>
-                  Research pricing points of various competitors and trial different business models
+                 Research pricing points of various competitors and trial different business models
                 </HeadingXL>
                 <HeadingS style={{ color: "#828FA3", margin:"24px 0"}}>
                   We know what we're planning to build for version one. Now we need to finalise
                   the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.
                 </HeadingS>
-                            </ModalContent>
-                        </ModalWrapper>
+                <HeadingM style={{ color: themeMode.text}}>
+                  Subtasks (2 of 3)
+                </HeadingM>
+                  </ModalContent>
+              </ModalWrapper>
                 </Background>
             ) : null}
         </>
